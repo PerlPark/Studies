cd1df847c21a45be878d768c1b29d87de6e58f00
perlpark <hello@jinjoo.me>
2024-02-12 17:16:06 +0900

    webpack-react-ts: file-loader 설치 및 적용

--- webpack-react-ts/webpack.config.js
+++ webpack-react-ts/webpack.config.js
@@ -8,6 +8,24 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  module: {
+		rules: [
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images/',
+              publicPath: 'images/',
+              esModule: false
+						}
+					}
+				]
+			}
+		]
+	},
   plugins: [
     new MyWebpackPlugin(),
     new CleanWebpackPlugin()
